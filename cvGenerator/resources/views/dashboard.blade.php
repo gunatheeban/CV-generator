629a210e5e1f8b89a24cd3afb8801c0b3dac7bfe
Gunatheeban <gunatheeban@gmail.com>
2022-05-02 21:59:36 +0530

    front end UI update

--- cvGenerator/resources/views/dashboard.blade.php
+++ cvGenerator/resources/views/dashboard.blade.php
@@ -8,7 +8,50 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+
+                    <div class="text-2xl">
+                        Hi {{ auth()->user()->name }},  Welcome to {{config("app.name")}}!
+                    </div>
+                </div>
+
+                <div class="bg-white bg-opacity-25 grid grid-cols-1 md:grid-cols-2">
+
+                    <div class="p-6 border-t border-gray-200">
+
+                    </div>
+
+                    <div class="p-6 border-t border-gray-200 md:border-l">
+                        <div class="mb-6 bg-gray-200 overflow-hidden shadow-xl sm:rounded-lg" >
+                            <div class="p-6 w-full">
+                                <b>Contact Information</b>
+                                <x-jet-button class="float-right">
+                                    <i class="fa fa-pencil" aria-hidden="true"></i>
+                                </x-jet-button>
+                            </div>
+                        </div>
+
+                        <div class="mb-6 bg-gray-200 overflow-hidden shadow-xl sm:rounded-lg" >
+                            <div class="p-6 w-full">
+                                <b>Contact Information</b>
+                                <x-jet-button class="float-right">
+                                    <i class="fa fa-pencil" aria-hidden="true"></i>
+                                </x-jet-button>
+
+                                <x-jet-button class=" mr-2  float-right">
+                                    <i class="fa fa-trash" aria-hidden="true"></i>
+                                </x-jet-button>
+                            </div>
+                        </div>
+
+                        <div class="mb-6 bg-gray-200 overflow-hidden shadow-xl sm:rounded-lg" >
+                            <div class="p-6">
+                                <b>Contact Information</b>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
